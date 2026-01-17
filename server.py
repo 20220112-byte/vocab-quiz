4d5c47a34fdc1cb2401edfa00464e224b4c37c5f
20220112-byte <20220112@st.shudo-h.ed.jp>
2026-01-17 17:28:15 +0900

    Add files via upload

--- server.py
+++ server.py
@@ -46,3 +46,10 @@ def check(d: dict):
 @app.get("/favicon.ico")
 def favicon():
     return FileResponse("static/favicon.ico")  # 置いてないならこの関数は消してOK
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
+
